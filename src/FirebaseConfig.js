d8101fdaba93402ba69f6bf2ce40abe52989f6eb
jonisland <jogucareers@gmail.com>
2022-08-19 20:56:24 -0700

    added username and password input and submit button

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -6,6 +6,7 @@ import "firebase/compat/storage";
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
@@ -16,3 +17,5 @@ const config = {
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
+
+export default firebase;
